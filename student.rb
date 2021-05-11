83e18f5748f5c546193314bdae00429b7838ef40
Ealian Kowalsky <ealiank@icloud.com>
2021-05-10 20:04:04 -0400

    Creating a student class

--- student.rb
+++ student.rb
@@ -1,1 +1,16 @@
-puts "student"
+class Student
+  def initialize
+  end
+
+  def study
+    puts "MATH!!!!!!!"
+  end
+
+  def read
+    puts "ERAGON!!!!!"
+  end
+
+  def write
+    puts "STUFF!!!"
+  end
+end
